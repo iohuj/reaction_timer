b449e155e858b73c03837fb1bd144b830b0654a4
iohuj <juho.inkinen@gmail.com>
2018-02-14 19:56:29 +0200

    Separate avg. reaction times for audio and visual stimulae.

--- reaction_timer.py
+++ reaction_timer.py
@@ -2,8 +2,9 @@ import time
 import numpy as np
 from random import randrange, randint
 
-reaction_time = []
-for i in range(5):
+reaction_time = np.array([])
+audio_stimulus = np.array([], dtype=bool)
+for i in range(2):
     print('ready...')
     print('2')
     time.sleep(1)
@@ -16,13 +17,23 @@ for i in range(5):
     t0 = time.time()
     if randint(0, 1):
         print('\a')  # Beep
+        audio_stimulus = np.append(audio_stimulus, True)
     else:
         print('REACT!!!')
+        audio_stimulus = np.append(audio_stimulus, False)
     input()
-    reaction_time.append(time.time() - t0)
+    reaction_time = np.append(reaction_time, time.time() - t0)
     print('Reaction time: ')
     print(str(reaction_time[-1]) + ' s' )
     print()
-    
+
+ 
 print()
-print('Avg. reaction time: {0:.3f} s, STD {1:.3f}'.format(np.mean(reaction_time), np.std(reaction_time)))
+print('Audio stimulus:')
+print('Avg. reaction time: {0:.3f} s, STD {1:.3f}'.format(
+        reaction_time[audio_stimulus].mean(), 
+        reaction_time[audio_stimulus].std()))
+print('Visual stimulus:')
+print('Avg. reaction time: {0:.3f} s, STD {1:.3f}'.format(
+        reaction_time[~audio_stimulus].mean(), 
+        reaction_time[~audio_stimulus].std()))
